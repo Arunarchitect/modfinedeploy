4e75444b97962c4d4df92aebe033977a5ef18158
Arunarchitect <intformbase@gmail.com>
2023-07-26 10:28:40 +0530

    setup deploy sh code

--- bin/set.sh
+++ bin/set.sh
@@ -1,6 +1,7 @@
-activate(){
+python3 -m venv venv
+activate() {
     . venv/bin/activate
+    echo "installing requirements to virtual environment"
+    pip install -r requirements.txt
 }
 activate
-
-
